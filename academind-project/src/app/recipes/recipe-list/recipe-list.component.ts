9ddb6afc45bfa9374abd3334896b18945381b7ff
kelbrad7 <98112140+kelbrad7@users.noreply.github.com>
2022-03-27 16:45:01 -0500

    continued work on angular project

--- academind-project/src/app/recipes/recipe-list/recipe-list.component.ts
+++ academind-project/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { TestBed } from '@angular/core/testing';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +8,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-
+  recipes: Recipe[] = [
+  new Recipe("a test recipe", "this is a test","https://image.shutterstock.com/image-photo/notepad-your-recipe-herbs-spices-260nw-370298699.jpg")
+  ];
   constructor() { }
 
   ngOnInit(): void {
