6b88f536f7d68b3621dc8e651bcdefd8ba5ccc34
kelbrad7 <98112140+kelbrad7@users.noreply.github.com>
2022-03-30 15:31:11 -0500

    navbar function

--- academind-project/src/app/header/header.component.ts
+++ academind-project/src/app/header/header.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+
+
 
 @Component({
   selector: 'app-header',
@@ -7,6 +9,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  @Output() featureSelected = new EventEmitter<string>();
+
+  onSelect (feature: string) {
+    this.featureSelected.emit (feature);
+
+  }
   constructor() { }
 
   ngOnInit(): void {
