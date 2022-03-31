65932ca097657c6b3e551b3ed7ba41354b609734
kelbrad7 <98112140+kelbrad7@users.noreply.github.com>
2022-03-30 20:28:33 -0500

    event emitters

--- academind-project/src/app/recipes/recipe-list/recipe-list.component.ts
+++ academind-project/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter, Input } from '@angular/core';
 import { TestBed } from '@angular/core/testing';
+
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -8,13 +9,20 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
+
+
   recipes: Recipe[] = [
-  new Recipe("a test recipe", "this is a test","https://image.shutterstock.com/image-photo/notepad-your-recipe-herbs-spices-260nw-370298699.jpg"),
-  new Recipe("another recipe", "this is another test","https://image.shutterstock.com/image-photo/notepad-your-recipe-herbs-spices-260nw-370298699.jpg")
+    new Recipe("a test recipe", "this is a test","https://image.shutterstock.com/image-photo/notepad-your-recipe-herbs-spices-260nw-370298699.jpg"),
+    new Recipe("another recipe", "this is another test","https://image.shutterstock.com/image-photo/notepad-your-recipe-herbs-spices-260nw-370298699.jpg")
   ];
+
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+  }
+  onRecipeSelected (recipe: Recipe){
+this.recipeWasSelected.emit(recipe);
   }
 
 }
