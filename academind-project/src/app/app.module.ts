b988c85a04ec97a1fae0e7475dfd644e014451ee
kelbrad7 <98112140+kelbrad7@users.noreply.github.com>
2022-04-07 09:22:50 -0500

    ingredient adding

--- academind-project/src/app/app.module.ts
+++ academind-project/src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
 import { RecipesComponent } from './recipes/recipes.component';
 import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
@@ -9,6 +8,8 @@ import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-it
 import { HeaderComponent } from './header/header.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
+import { ViewChild } from '@angular/core';
+import { ElementRef } from '@angular/core';
 
 
 @NgModule({
--- academind-project/src/app/shopping-list/shopping-list.component.ts
+++ academind-project/src/app/shopping-list/shopping-list.component.ts
@@ -8,14 +8,17 @@ import { Ingredient } from '../shared/ingredient.model';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
-  ingredients = [] = [
-    new Ingredient ('apples', 5),
-    new Ingredient ("tomatoes", 10)
+  ingredients: Ingredient[] = [
+    new Ingredient('Apples', 5),
+    new Ingredient('Tomatoes', 10),
   ];
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit() {
   }
 
+  onIngredientAdded(ingredient: Ingredient) {
+    this.ingredients.push(ingredient);
+  }
 }
